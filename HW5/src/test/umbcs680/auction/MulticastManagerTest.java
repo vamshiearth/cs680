e29b1b0a3a2fe142103dd3c6b67e82818e95d050
Vamshi <73492940+vamshiearth@users.noreply.github.com>
2025-03-12 23:09:12 -0400

    Create MulticastManagerTest.java

---
+++ HW5/src/test/umbcs680/auction/MulticastManagerTest.java
@@ -0,0 +1,32 @@
+package umbcs680.auction;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MulticastManagerTest {
+
+    @Test
+    void subscribeToBids() {
+    }
+
+    @Test
+    void subscribeToAuctionEnd() {
+    }
+
+    @Test
+    void unsubscribeFromBids() {
+    }
+
+    @Test
+    void unsubscribeFromAuctionEnd() {
+    }
+
+    @Test
+    void notifyBidObservers() {
+    }
+
+    @Test
+    void notifyEndObservers() {
+    }
+}
