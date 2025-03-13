3ab574dbfaa06e5497e1ab4c2f33465dcf5c5273
Vamshi <73492940+vamshiearth@users.noreply.github.com>
2025-03-12 21:40:07 -0400

    Updated Auction Test

---
+++ HW5/src/test/umbcs680/auction/BidderTest.java
@@ -0,0 +1,23 @@
+package umbcs680.auction;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class BidderTest extends TestCase {
+
+    @Test
+    public void testSubscribeToAuction() {
+    }
+
+    @Test
+    public void testUnsubscribeFromAuction() {
+    }
+
+    @Test
+    public void testUpdate() {
+    }
+
+    @Test
+    public void testTestGetName() {
+    }
+}
