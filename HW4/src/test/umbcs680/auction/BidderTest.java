689d362c9136c60abea2ca680900ab9993362aeb
Vamshi <73492940+vamshiearth@users.noreply.github.com>
2025-03-12 22:59:10 -0400

    Update BidderTest.java

--- HW4/src/test/umbcs680/auction/BidderTest.java
+++ HW4/src/test/umbcs680/auction/BidderTest.java
@@ -1,16 +1,27 @@
 package umbcs680.auction;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class BidderTest {
 
     @Test
     void update() {
+        Auction auction = new Auction("Laptop");
+        Bidder bidder = new Bidder("Dave");
+
+        auction.addObserver(bidder);
+        auction.placeBid(300);
+
+        assertEquals("New highest bid of 300.0 on Laptop", bidder.getLastMessage());
     }
 
     @Test
     void getLastMessage() {
+        Bidder bidder = new Bidder("Eve");
+        assertNull(bidder.getLastMessage(), "Last message should be null initially.");
+
+        bidder.update(null, "Test notification");
+        assertEquals("Test notification", bidder.getLastMessage(), "Last message should be updated correctly.");
     }
 }
