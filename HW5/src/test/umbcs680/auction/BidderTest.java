b901edd4b59a4c7a54fe8b2bfe43507e5e913723
Vamshi <73492940+vamshiearth@users.noreply.github.com>
2025-03-12 21:45:42 -0400

    Updated BIdderTest

--- HW5/src/test/umbcs680/auction/BidderTest.java
+++ HW5/src/test/umbcs680/auction/BidderTest.java
@@ -1,23 +1,42 @@
 package umbcs680.auction;
 
-import junit.framework.TestCase;
-import org.junit.Test;
 
-public class BidderTest extends TestCase {
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class BidderTest {
 
     @Test
     public void testSubscribeToAuction() {
+        Auction auction = new Auction("Painting");
+        Bidder bidder = new Bidder("Alice");
+
+        bidder.subscribeToAuction(auction);
+        // No direct assertion, but we assume subscription works if no errors occur.
     }
 
     @Test
     public void testUnsubscribeFromAuction() {
+        Auction auction = new Auction("Sculpture");
+        Bidder bidder = new Bidder("Bob");
+
+        bidder.subscribeToAuction(auction);
+        bidder.unsubscribeFromAuction(auction);
+        // No direct assertion, but we assume unsubscription works if no errors occur.
     }
 
     @Test
     public void testUpdate() {
+        Auction auction = new Auction("Car");
+        Bidder bidder = new Bidder("Charlie");
+
+        bidder.update(auction, "New bid placed!");
+        // Since update prints a message, we can't assert it directly.
     }
 
     @Test
-    public void testTestGetName() {
+    public void testGetName() {
+        Bidder bidder = new Bidder("David");
+        assertEquals("David", bidder.getName());
     }
 }
---
+++ HW5/src/test/umbcs680/auction/MulticastManagerTest.java
@@ -0,0 +1,18 @@
+package umbcs680.auction;
+
+import org.junit.Test;
+
+public class MulticastManagerTest{
+
+    @Test
+    public void testSubscribe() {
+    }
+
+    @Test
+    public void testUnsubscribe() {
+    }
+
+    @Test
+    public void testNotifyBidders() {
+    }
+}
