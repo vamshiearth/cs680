58b41e67f57d8c07e90fcbcb1a245abacd073349
Vamshi <73492940+vamshiearth@users.noreply.github.com>
2025-03-12 21:48:02 -0400

    Updated MulticastManagerTest

--- HW5/src/test/umbcs680/auction/MulticastManagerTest.java
+++ HW5/src/test/umbcs680/auction/MulticastManagerTest.java
@@ -2,17 +2,37 @@ package umbcs680.auction;
 
 import org.junit.Test;
 
-public class MulticastManagerTest{
+public class MulticastManagerTest {
 
     @Test
     public void testSubscribe() {
+        Auction auction = new Auction("Watch");
+        Bidder bidder = new Bidder("Eve");
+
+        MulticastManager.subscribe(auction, bidder);
+        // No direct assertion, assuming subscription works if no errors occur.
     }
 
     @Test
     public void testUnsubscribe() {
+        Auction auction = new Auction("Painting");
+        Bidder bidder = new Bidder("Frank");
+
+        MulticastManager.subscribe(auction, bidder);
+        MulticastManager.unsubscribe(auction, bidder);
+        // No direct assertion, assuming unsubscription works if no errors occur.
     }
 
     @Test
     public void testNotifyBidders() {
+        Auction auction = new Auction("Laptop");
+        Bidder bidder1 = new Bidder("Grace");
+        Bidder bidder2 = new Bidder("Henry");
+
+        MulticastManager.subscribe(auction, bidder1);
+        MulticastManager.subscribe(auction, bidder2);
+
+        MulticastManager.notifyBidders(auction, "Test notification");
+        // Since notification is printed, we can't assert directly.
     }
 }
