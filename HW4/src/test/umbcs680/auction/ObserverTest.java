64448f26a8b9827aadff6f45d30f16512f179e7d
Vamshi <73492940+vamshiearth@users.noreply.github.com>
2025-03-12 23:00:03 -0400

    Update ObserverTest.java

--- HW4/src/test/umbcs680/auction/ObserverTest.java
+++ HW4/src/test/umbcs680/auction/ObserverTest.java
@@ -1,12 +1,18 @@
 package umbcs680.auction;
 
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class ObserverTest {
 
     @Test
     void update() {
+        Auction auction = new Auction("Game Console");
+        Bidder bidder = new Bidder("Jack");
+
+        auction.addObserver(bidder);
+        auction.notifyObservers("New bid placed: $500");
+
+        assertEquals("New bid placed: $500", bidder.getLastMessage(), "Bidder should receive the correct update message.");
     }
 }
